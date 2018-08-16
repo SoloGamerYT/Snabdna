8d564c479e00777b9e3a7cc32692753f4da7198d
SoloGamerYT <41999799+SoloGamerYT@users.noreply.github.com>
2018-08-16 17:44:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1329,9 +1329,21 @@ client.on('message', message => {
   var argresult = args.join(' ');
   if (message.author.id !== "439187325503930369") return;
 
+if (message.content.startsWith(PREFIX + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
+	 console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}`)
+}
+
+if (message.content.startsWith(PREFIX + 'setname')) {
+  client.user.setUsername(argresult).then
+	  message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+}
+if (message.content.startsWith(PREFIX + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 });
-
-
 
 client.login(process.env.BOT_TOKEN);
