6d84ca31a8f5731218879251a93c49efa3d57b2f
SoloGamerYT <41999799+SoloGamerYT@users.noreply.github.com>
2018-08-20 13:18:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1327,8 +1327,6 @@ if (message.content.startsWith(PREFIX + 'setname')) {
 if (message.content.startsWith(PREFIX + 'setavatar')) {
   client.user.setAvatar(argresult);
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
-}
-});
 
 
 
